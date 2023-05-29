525cda9891dfe7a555ec90489b73c0fb19793990
TwoTonne <arthur.costa@outlook.com.br>
2023-05-28 23:20:32 -0300

    Alteração do Arthur

--- BlackJack-Git/src/BlackJack_Code/Main.java
+++ BlackJack-Git/src/BlackJack_Code/Main.java
@@ -2,7 +2,7 @@ package BlackJack_Code;
 public class Main {
     public static void main(String[] args) {
         
-        System.out.println("Arthur aqui");
+        System.out.println("Arthu aqui");
         
         
     }
