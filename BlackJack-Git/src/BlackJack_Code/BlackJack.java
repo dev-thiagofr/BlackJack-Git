1fdf5f2ef8c1d72d5ba7f64e56e0bbb91aa4cb2f
dev-Thiago <thiagofr.029@gmail.com>
2023-06-04 15:47:20 -0300

    Construindo a Classe BlackJack. Implementação das construções da mão inicial e adicionandoa a comparação de resultados.

--- BlackJack-Git/src/BlackJack_Code/BlackJack.java
+++ BlackJack-Git/src/BlackJack_Code/BlackJack.java
@@ -1,4 +1,45 @@
 package BlackJack_Code;
 public class BlackJack {
+    private Baralho baralhodoJogo;
+    private Jogadores dealer;
+    private Jogadores player;
     
+    public BlackJack(){
+        baralhodoJogo = new Baralho();
+        //baralhodoJogo.embaralhar();
+        
+        dealer = new Jogadores();
+        player = new Jogadores();
+        
+    }
+    public void Jogo(){
+        //Formação da mão inicial;
+        player.adicionarCarta(baralhodoJogo.compraCarta());
+        dealer.adicionarCarta(baralhodoJogo.compraCarta());
+        
+        player.adicionarCarta(baralhodoJogo.compraCarta());
+        dealer.adicionarCarta(baralhodoJogo.compraCarta());
+    }
+    
+    public void resultados(){
+        int pPontos = player.valorDaMão();
+        int dPontos = dealer.valorDaMão();
+        
+        if(pPontos>21){
+            System.out.println("Você estourou , o dealer ganha");
+        }
+        else if(dPontos>21){
+            System.out.println("Que sorte! O dealer estourou , voce ganhou!");
+        }
+        else if(dPontos>pPontos){
+            System.out.println("O dealer ganhou!");
+        }
+        else if(dPontos<pPontos){
+            System.out.println("Você venceu!");
+        }
+        else{
+            System.out.println("Empate.");
+        }
+        
+    }
 }
